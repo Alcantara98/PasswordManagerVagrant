aa9e7e21d956e66fa8cf74118b2774182c5a3ec9
Elshtein99 <57881656+Elshtein99@users.noreply.github.com>
2020-08-26 01:55:45 +1200

    sign up and login html and php done
    Need to add CSS once all webpages are done and works in vagrant VM.

--- setup-database.sql
+++ setup-database.sql
@@ -7,12 +7,19 @@ CREATE TABLE papers (
 INSERT INTO papers VALUES ('COSC326','Effective Programming');
 INSERT INTO papers VALUES ('COSC349','Cloud Computing Architecture');
 
+DROP TABLE users;
+DROP TABLE rootusers;
+
 CREATE TABLE users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO papers VALUES('viperzz33', 'hellokitty');
-INSERT INTO papers VALUES('elbert', 'alcantara');
+CREATE TABLE rootusers (
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users VALUES('viperzz33', 'hellokitty');
+INSERT INTO users VALUES('elbert', 'alcantara');
+INSERT INTO rootusers VALUES('elbert', 'alcantara');
--- www/login.php
+++ www/login.php
@@ -2,11 +2,10 @@
 <html lang="en" dir="ltr">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>PASSWORD login</title>
 </head>
 <body>
-
-    <p>Insecure Password Keeper</p>
+    <h1>Insecure Password Manager Login</h1>
     <a href="signup.php">Sign Up</a>
     <br>
     <br>
@@ -18,23 +17,56 @@
         <br>
         <label>Password:</label>
         <br>
-        <INPUT TYPE = "Text" VALUE ="" NAME = "password">
+        <INPUT TYPE = "Text" VALUE ="" NAME = "dpassword">
         <br>
         <br>
         <INPUT TYPE = "Submit" Name = "Submit1" VALUE = "Login">
     </form>
     <br>
     <?php
+    //imports our connection to database server from dbconnection.php
     require 'dbconnection.php';
+
+    //This section checks if one or both boxes are blank and notifies the user.
     if(isset($_POST['username']) && $_POST['username'] === '' 
-    && isset($_POST['password']) && $_POST['password'] !== ''){
+    && isset($_POST['dpassword']) && $_POST['dpassword'] !== ''){
         echo 'please fill in username box'; 
     }elseif(isset($_POST['username']) && $_POST['username'] !== '' 
-    && isset($_POST['password']) && $_POST['password'] === ''){
+    && isset($_POST['dpassword']) && $_POST['dpassword'] === ''){
         echo 'please fill in password box';
-    }elseif(isset($_POST['username']) && $_POST['username'] !== '' 
-    && isset($_POST['password']) && $_POST['password'] !== ''){
+    }elseif(isset($_POST['username']) && $_POST['username'] === '' 
+    && isset($_POST['dpassword']) && $_POST['dpassword'] === ''){
+        echo 'please fill in username and password box';
+    }
+    //If both are filled we go ahead with the database query.
+    elseif(isset($_POST['username']) && $_POST['username'] !== '' 
+    && isset($_POST['dpassword']) && $_POST['dpassword'] !== ''){
+        $username = $_POST['username'];
+        $dpassword = $_POST['dpassword'];
 
+        //Instead of putting the variables straight in, we this to avoid sql injections.
+        $sql = "SELECT * FROM users WHERE username = ? && password = ?;";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            exit();
+        }else{
+            mysqli_stmt_bind_param($stmt, "ss", $username, $dpassword);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+
+            //If query succesful, we check again just incase, then we start a session.
+            if($row = mysqli_fetch_array($result)){
+                if($dpassword == $row['password'] && $username == $row['username']){
+                    session_start();
+                    $_SESSION['unique_username'] = $row['username'];
+                    header('Location: passwords.php?Login=successful');
+                }
+            }
+ 
+            else{
+                echo "invalid username or password";
+            }
+        }
     }
     ?>
 </body>
