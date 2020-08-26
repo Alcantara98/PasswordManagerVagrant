105eaa1305bdc9576cb9d5ccba5ce6ea61498cd9
Elshtein99 <57881656+Elshtein99@users.noreply.github.com>
2020-08-27 01:56:07 +1200

    user php pages are all done
    No css atm, may add it later on.
    Need to work on rootuser webserver and query php pages for rootusers next.

--- www/login.php
+++ www/login.php
@@ -1,9 +1,19 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
+<?php
+if (isset($_SESSION['unique_username'])) {
+    header("Location: passwords.php?currently_logged_in");
+}
+?>
 <html lang="en" dir="ltr">
+
 <head>
     <meta charset="UTF-8">
     <title>PASSWORD login</title>
 </head>
+
 <body>
     <h1>Insecure Password Manager Login</h1>
     <a href="signup.php">Sign Up</a>
@@ -12,78 +22,60 @@
     <form action="" method="post">
         <label>Username:</label>
         <br>
-        <INPUT TYPE = "Text" VALUE ="" NAME = "username">
+        <INPUT TYPE="Text" VALUE="" NAME="username">
         <br>
         <br>
         <label>Password:</label>
         <br>
-        <INPUT TYPE = "Text" VALUE ="" NAME = "dpassword">
+        <INPUT TYPE="Text" VALUE="" NAME="dpassword">
         <br>
         <br>
-        <INPUT TYPE = "Submit" Name = "Submit1" VALUE = "Login">
+        <INPUT TYPE="Submit" Name="Submit1" VALUE="Login">
     </form>
-    <br>
     <?php
-    session_start();
     //imports our connection to database server from dbconnection.php
     require 'dbconnection.php';
 
     //This section checks if one or both boxes are blank and notifies the user.
-    if(isset($_POST['username']) && $_POST['username'] === '' 
-    && isset($_POST['dpassword']) && $_POST['dpassword'] !== ''){
-        echo 'please fill in username box'; 
-    }elseif(isset($_POST['username']) && $_POST['username'] !== '' 
-    && isset($_POST['dpassword']) && $_POST['dpassword'] === ''){
-        echo 'please fill in password box';
-    }elseif(isset($_POST['username']) && $_POST['username'] === '' 
-    && isset($_POST['dpassword']) && $_POST['dpassword'] === ''){
-        echo 'please fill in username and password box';
-    }
-    //If both are filled we go ahead with the database query.
-    elseif(isset($_POST['username']) && $_POST['username'] !== '' 
-    && isset($_POST['dpassword']) && $_POST['dpassword'] !== ''){
-        $dusername = $_POST['username'];
-        $dpassword = $_POST['dpassword'];
-        /*
-        //This will allow injections, prepare function doesn't seem to work with current sql server.
-        $sql = "SELECT * FROM users WHERE username = '$dusername' AND password = '$dpassword';";
-        $result = mysqli_query($conn, $sql);
-
-        if($row = mysqli_fetch_array($result)){
-            if($dpassword == $row['password'] && $dusername == $row['username']){
-                $_SESSION['unique_username'] = $row['username'];
-                header('Location: passwords.php?Login=successful');
-            }
+    if (isset($_POST['username']) && isset($_POST['dpassword'])) {
+        if ($_POST['username'] === '' && $_POST['dpassword'] !== '') {
+            echo 'please fill in username box';
+        } elseif ($_POST['username'] !== '' && $_POST['dpassword'] === '') {
+            echo 'please fill in password box';
+        } elseif ($_POST['username'] === '' && $_POST['dpassword'] === '') {
+            echo 'please fill in username and password box';
         }
-        else{
-            echo "invalid username or password";
-        }*/
-        
-        //Instead of putting the variables straight in, we this to avoid sql injections.
-        $sql = "SELECT * FROM users WHERE username = ? && password = ?;";
-        $stmt = mysqli_stmt_init($conn);
-        if(!mysqli_stmt_prepare($stmt, $sql)){
-            echo "Hello";
-            exit();
-        }else{
-            mysqli_stmt_bind_param($stmt, "ss", $dusername, $dpassword);
-            mysqli_stmt_execute($stmt);
-            $result = mysqli_stmt_get_result($stmt);
+        //If both are filled we go ahead with the database query.
+        elseif ($_POST['username'] !== '' && $_POST['dpassword'] !== '') {
+            $dusername = $_POST['username'];
+            $dpassword = $_POST['dpassword'];
 
-            //If query succesful, we check again just incase, then we start a session.
-            if($row = mysqli_fetch_array($result)){
-                if($dpassword == $row['password'] && $dusername == $row['username']){
-                    session_start();
-                    $_SESSION['unique_username'] = $row['username'];
-                    header('Location: passwords.php?Login=successful');
+            //Instead of putting the variables straight in, we this to avoid sql injections.
+            $sql = "SELECT * FROM users WHERE username = ? && password = ?;";
+            $stmt = mysqli_stmt_init($conn);
+            if (!mysqli_stmt_prepare($stmt, $sql)) {
+                exit();
+            } else {
+                mysqli_stmt_bind_param($stmt, "ss", $dusername, $dpassword);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+
+                //If query succesful, we check again just incase, then we start a session.
+                if ($row = mysqli_fetch_array($result)) {
+                    if ($dpassword == $row['password'] && $dusername == $row['username']) {
+                        session_start();
+                        $_SESSION['unique_username'] = $row['username'];
+                        header('Location: passwords.php?Login=successful');
+                        exit();
+                    }
+                } else {
+                    echo "invalid username or password";
+                    exit();
                 }
-            }
- 
-            else{
-                echo "invalid username or password";
             }
         }
     }
     ?>
 </body>
-</html> 
+
+</html>
--- www/passwords.php
+++ www/passwords.php
@@ -1,11 +1,140 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
+<?php
+if (!isset($_SESSION['unique_username'])) {
+    header("Location: login.php?please_login");
+    exit();
+}
+?>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Password Manager</title>
 </head>
+
 <body>
-    
+    <h1>Password Manager</h1>
+    <a href="logout.php">Sign out</a>
+    <br>
+    <p>Fill in the following three fields to add account to your Password Manager</p>
+    <form action="" method="post">
+        <label>Website:</label>
+        <br>
+        <input type="hidden" name="step" value="1">
+        <INPUT TYPE="Text" VALUE="" NAME="dwebsite">
+        <br>
+        <br>
+        <label>Username:</label>
+        <br>
+        <INPUT TYPE="Text" VALUE="" NAME="dusername">
+        <br>
+        <br>
+        <label>Password:</label>
+        <br>
+        <INPUT TYPE="Text" VALUE="" NAME="dpassword">
+        <br>
+        <br>
+        <INPUT TYPE="Submit" Name="Submit1" VALUE="Add">
+    </form>
+    <?php
+    require 'dbconnection.php';
+    if (isset($_POST['step']) && $_POST['step'] == 1) {
+        if (isset($_POST['dwebsite']) && isset($_POST['dusername']) && isset($_POST['dpassword'])) {
+            if ($_POST['dwebsite'] !== '' && $_POST['dusername'] !== '' && $_POST['dpassword'] !== '') {
+
+                $websiteone = $_POST['dwebsite'];
+                $usernameone = $_POST['dusername'];
+                $password = $_POST['dpassword'];
+
+                $sql = "SELECT * FROM useraccounts WHERE website = ? AND username = ? AND password = ?;";
+                $stmt = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    echo "Hello0";
+                    exit();
+                } else {
+                    mysqli_stmt_bind_param($stmt, "sss", $websiteone, $usernameone, $password);
+                    mysqli_stmt_execute($stmt);
+                    $result = mysqli_stmt_get_result($stmt);
+
+                    //If query succesful, we check again just incase, then we start a session.
+                    if ($row = mysqli_fetch_array($result)) {
+                        if ($websiteone == $row['website'] && $usernameone == $row['username'] && $password == $row['password']) {
+                            echo "account is already saved";
+                        }
+                    } else {
+                        $sql = "INSERT INTO useraccounts (website, username, password) VALUES (?, ?, ?);";
+                        $stmt = mysqli_stmt_init($conn);
+                        if (!mysqli_stmt_prepare($stmt, $sql)) {
+                            exit();
+                        } else {
+                            mysqli_stmt_bind_param($stmt, "sss", $websiteone, $usernameone, $password);
+                            mysqli_stmt_execute($stmt);
+                            echo "account has been added";
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    ?>
+    <p>Fill in the following two fields to retrieve password</p>
+    <form action="" method="post">
+        <label>Website:</label>
+        <br>
+        <input type="hidden" name="step" value="2">
+        <INPUT TYPE="Text" VALUE="" NAME="dwebsite">
+        <br>
+        <br>
+        <label>Username:</label>
+        <br>
+        <INPUT TYPE="Text" VALUE="" NAME="dusername">
+        <br>
+        <br>
+        <INPUT TYPE="Submit" Name="Submit1" VALUE="Retrieve">
+        <br>
+    </form>
+
+    <?php
+    require 'dbconnection.php';
+    if (isset($_POST['step']) && $_POST['step'] == 2) {
+        if (isset($_POST['dwebsite']) && isset($_POST['dusername'])) {
+            if ($_POST['dwebsite'] !== '' && $_POST['dusername'] !== '') {
+
+                $websitetwo = $_POST['dwebsite'];
+                $usernametwo = $_POST['dusername'];
+
+                $sql = "SELECT * FROM useraccounts WHERE website = ? AND username = ?";
+                $stmt = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    exit();
+                } else {
+                    mysqli_stmt_bind_param($stmt, "ss", $websitetwo, $usernametwo);
+                    mysqli_stmt_execute($stmt);
+                    $result = mysqli_stmt_get_result($stmt);
+
+                    //If query succesful, we check again just incase, then we start a session.
+                    if ($row = mysqli_fetch_array($result)) {
+                        if ($websitetwo == $row['website'] && $usernametwo == $row['username']) {
+                            $passwd = $row['password'];
+                            echo "Password: " . $passwd;
+                            exit();
+                        } else {
+                            echo "account does not exist";
+                            exit();
+                        }
+                    } else {
+                        echo "account does not exist";
+                        exit();
+                    }
+                }
+            }
+        }
+    }
+
+    ?>
 </body>
+
 </html>
